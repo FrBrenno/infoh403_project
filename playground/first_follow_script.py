6b86cbeb404efa9ea21c6a9dddd0d23367030a3e
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-11-17 22:38:41 +0100

    Correction Follow + logging - detailed computation

--- playground/first_follow_script.py
+++ playground/first_follow_script.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import csv
+import datetime
 
 
 class Variables(Enum):
@@ -134,27 +135,42 @@ grammar = {
            }
 
 first_sets = {}
+logging_first = []
 follow_sets = {}
+logging_follow = []
 
 """This function computes the first set of a given variable in the grammar.
 """
 def first(variable):
-    if first_sets.get(variable) is not None:  # Base case: first set is already computed
+    logging_first.append(f"(INFO): Calculating first set for variable {variable}")
+    # Base case: first set is already computed
+    if first_sets.get(variable) is not None:
+        logging_first.append(f"(DEBUG): Base case: First set for {variable} already computed: {first_sets[variable]}")
         return first_sets[variable]
 
     first_sets[variable] = set()
 
-    if variable in Terminals:  # Base case: variable is a terminal
+    # Base case: variable is a terminal
+    if variable in Terminals:
         first_sets[variable].add(variable)
+        logging_first.append(f"(DEBUG): Base case: Found terminal {variable} in variable {variable}")
         return first_sets[variable]
 
+    logging_first.append(f"(DEBUG): Calculating first set for variable {variable}")
+
     for production in grammar[variable]:
-        if production[0] in Terminals:
-            first_sets[variable].add(production[0])
+        first_symbol = production[0]
+        if first_symbol in Terminals:
+            logging_first.append(f"(DEBUG): First Symbol of production {production} is terminal {first_symbol}")
+            first_sets[variable].add(first_symbol)
         else:
-            first_sets[variable] = first_sets[variable].union(first(production[0]))
-            if Terminals.EPSILON in first(production[0]):
+            logging_first.append(f"(DEBUG): First Symbol of production {production} is variable {first_symbol}")
+            first_sets[variable] = first_sets[variable].union(first(first_symbol))
+            if Terminals.EPSILON in first(first_symbol):
+                logging_first.append(f"(DEBUG): First Symbol of production {production} is variable {first_symbol} and epsilon is in first set of {first_symbol}")
                 first_sets[variable] = first_sets[variable].union(first(production[1]))
+
+    logging_first.append(f"(INFO):Computed first set for variable {variable}: {first_sets[variable]}")
 
     return first_sets[variable]
 
@@ -163,27 +179,55 @@ def first(variable):
 It avoids infinite recursion by keeping track of the variables for which the follow set is already computed.
 """
 def follow(variable):
-    if follow_sets.get(variable) is not None:  # Base case: follow set is already computed
+    logging_follow.append(f"(INFO): Calculating follow set for variable {variable}")
+    # Base case: follow set is already computed
+    if follow_sets.get(variable) is not None:
+        logging_follow.append(f"(DEBUG): Base case: Follow set for {variable} already computed: {follow_sets[variable]}")
         return follow_sets[variable]
 
     follow_sets[variable] = set()
 
-    if variable == Variables.PROGRAM:  # Base case: variable is the start symbol
+    # Base case: variable is the start symbol
+    if variable == Variables.PROGRAM:
+        logging_follow.append(f"(DEBUG): Base case: Found start symbol {variable}")
         follow_sets[variable].add(Terminals.EOF)
 
+    logging_follow.append(f"(DEBUG): Calculating follow set for variable {variable}")
+
     for key in grammar:
+        logging_follow.append(f"(DEBUG): ### Checking variable {key}")
         for production in grammar[key]:
             if variable in production:
-                if production.index(variable) == len(production) - 1:  # Case 1: variable is at the end of the production
+                variable_index = production.index(variable)
+                if variable_index == len(production) - 1:
+                    # Case 1: variable is at the end of the production
                     if key != variable:
+                        logging_follow.append(f"(DEBUG): Case 1: Found variable {variable} at the end of production {production}")
                         follow_sets[variable] = follow_sets[variable].union(follow(key))
-                else:  # Case 2: variable is not at the end of the production
-                    if production[production.index(variable) + 1] in Terminals:
-                        follow_sets[variable].add(production[production.index(variable) + 1])
+                else:
+                    # Case 2: variable is not at the end of the production
+                    next_symbol = production[variable_index + 1]
+                    if next_symbol in Terminals:
+                        # Case 2.1: next symbol is a terminal
+                        logging_follow.append(f"(DEBUG): Case 2.1: Found terminal {next_symbol} after variable {variable}")
+                        follow_sets[variable].add(next_symbol)
                     else:
-                        follow_sets[variable] = follow_sets[variable].union(first(production[production.index(variable) + 1]))
-                        if Terminals.EPSILON in first(production[production.index(variable) + 1]):
-                            follow_sets[variable] = follow_sets[variable].union(follow(key))
+                        # Case 2.2: next symbol is a variable
+                        if Terminals.EPSILON in first(next_symbol):
+                            # Case 2.2.1: epsilon is in the first set of the next symbol
+                            logging_follow.append(f"(DEBUG): Case 2.2.1: Found variable {next_symbol} after variable {variable} and epsilon is in first set of {next_symbol}")
+                            follow_sets[variable] = follow_sets[variable].union(first(next_symbol).difference({Terminals.EPSILON}))
+                            logging_follow.append(f"(DEBUG): Case 2.2.1: Consider applying rule with epsilon in the next iteration")
+                            follow_sets[variable] = follow_sets[variable].union(follow(next_symbol))
+                            if variable_index + 1 == len(production) - 1:
+                                logging_follow.append(f"(DEBUG): Case 2.2.1: Found variable {next_symbol} at the end of production {production}")
+                                follow_sets[variable] = follow_sets[variable].union(follow(key))
+                        else:
+                            # Case 2.2.2: epsilon is not in the first set of the next symbol
+                            logging_follow.append(f"(DEBUG): Case 2.2.2: Found variable {next_symbol} after variable {variable} and epsilon is not in first set of {next_symbol}")
+                            follow_sets[variable] = follow_sets[variable].union(first(next_symbol))
+
+    logging_follow.append(f"(INFO): Computed follow set for variable {variable}: {follow_sets[variable]}")
 
     return follow_sets[variable]
 
@@ -196,20 +240,14 @@ def action_table():
     """
     action = {}
     rule_number = 0
-def action_table():
-    """ 
-    This function computes the action table of this grammar. 
-    It gives the absolute number of the rule generating the terminal in the cell corresponding to the variable and the terminal.
-    Absolute number means the number of the rule in the grammar, not the number of the rule in the production.
-    """
-    action = {}
-    rule_number = 0
 
+    # Initialize action table
     for variable in Variables:
         action[variable] = {}
         for terminal in Terminals:
             action[variable][terminal] = None
 
+    # Fill action table
     for variable in grammar:
         for production in grammar[variable]:
             rule_number += 1
@@ -225,47 +263,145 @@ def action_table():
     return action
     
     
-def action_table_to_csv():
-    """ 
-    This function writes the action table to a csv file. 
-    """
-    action = action_table()
-    with open(r"C:\Users\brenn\IdeaProjects\infoh403_project\playground\action.csv", "w+", newline='') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerow([""] + [terminal.name for terminal in Terminals])
-        for key in action:
-            writer.writerow([key.name] + [action[key][terminal] for terminal in Terminals])
 
-def generate_csv():
-    with open(r"C:\Users\brenn\IdeaProjects\infoh403_project\playground\first.csv", "w+", newline='') as f:
+def first_follow_csv():
+    now = datetime.datetime.now()
+    first_csv_name = f".\\playground\\first_{now.day}_{now.month}_{now.hour}_{now.minute}.csv"
+    follow_csv_name = f".\\playground\\follow_{now.day}_{now.month}_{now.hour}_{now.minute}.csv"
+    
+    with open(first_csv_name, "w+", newline='') as f:
         writer = csv.writer(f, lineterminator='\n')
         writer.writerow(["Variable", "FIRST"])
         for key in grammar:
             first_set = first(key)
             writer.writerow([f"<{key.name}>",f"{[item.value[0] if isinstance(item, Enum) else item for item in first_set]}"])
 
-    with open(r"C:\Users\brenn\IdeaProjects\infoh403_project\playground\follow.csv", "w+", newline='') as f:
+    with open(follow_csv_name, "w+", newline='') as f:
         writer = csv.writer(f, lineterminator='\n')
         writer.writerow(["Variable", "FOLLOW"])
         for key in grammar:
             follow_set = follow(key)
             writer.writerow([f"<{key.name}>",f"{[item.value[0] if isinstance(item, Enum) else item for item in follow_set]}"])
 
+def action_table_to_csv():
+    """ 
+    This function writes the action table to a csv file. 
+    """
+    now = datetime.datetime.now()
+    action_csv_name = f".\\playground\\action_{now.day}_{now.month}_{now.hour}_{now.minute}.csv"
+    
+    action = action_table()
+    with open(action_csv_name, "w+", newline='') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow([""] + [terminal.name for terminal in Terminals])
+        for key in action:
+            writer.writerow([key.name] + [action[key][terminal] for terminal in Terminals])
+
+
+def check_ll1():
+    log = []
+    cond1 = True
+    cond2 = True
+    cond3 = True
+    is_ll1 = True
+
+    # Check condition 1: No common prefix
+    log.append(f"#"*20+" CONDITION 1 "+"#"*20)
+    log.append("Checking condition 1: No common prefix")
+    for non_terminal in grammar:
+        log.append("Checking non-terminal " + non_terminal.name)
+        productions = grammar[non_terminal]
+        prefixes = set()
+        for production in productions:
+            prefix = production[0]
+            if prefix in prefixes:
+                cond1 = False
+                log.append(f"Error: Common prefix '{prefix}' found in productions of {non_terminal}")
+            prefixes.add(prefix)
+    log.append("Condition 1: No common prefix check completed")
+    
+    if not cond1:
+        log.append("Condition 1: No common prefix check failed")
+        is_ll1 = False
+    else:
+        log.append("Condition 1: PASSED!")
+
+    # Check condition 2: No ε-productions conflict
+    log.append(f"#"*20+" CONDITION 2 "+"#"*20)
+    log.append("Checking condition 2: No ε-productions conflict")
+    for non_terminal in grammar:
+        log.append("Checking non-terminal " + non_terminal.name + " for ε-productions")
+        productions = grammar[non_terminal]
+        has_epsilon = any([] in production for production in productions)
+        if has_epsilon:
+            follow_set = follow(non_terminal)
+            for terminal in follow_set:
+                for production in productions:
+                    if production and production[0] == terminal:
+                        cond2 = False
+                        log.append(f"Error: Conflict found for ε-production of {non_terminal} and terminal '{terminal}'")
+    log.append("Condition 2: No ε-productions conflict check completed")
+    
+    if not cond2:
+        log.append("Condition 2: No ε-productions conflict check failed")
+        is_ll1 = False
+    else:
+        log.append("Condition 2: PASSED!")
+        
+
+    # Check if grammar is Strong LL(1) using first and follow set already computed
+    log.append(f"#"*20+" CONDITION bis "+"#"*20)
+    log.append("Checking if grammar is LL(1) using First and Follow sets")
+    for variable in grammar:
+        # Check if there is more than one production for the non-terminal
+        nb_rules = len(grammar[variable])
+        if nb_rules > 1:
+            log.append(f"### Checking non-terminal {variable.name} for LL(1)")
+            # For each pair of productions, check if the intersection of the first of the first production union the follow of the non-terminal and the first of the second production is empty
+            for i in range(nb_rules):
+                for j in range(i+1, nb_rules):
+                    first1 = first(grammar[variable][i][0])
+                    follow1 = follow(variable)
+                    first2 = first(grammar[variable][j][0])
+                    log.append(f"Checking rule: {variable.name} -> {grammar[variable][i][0]} | {grammar[variable][j][0]}")
+                    if first1.intersection(follow1).intersection(first2):
+                        cond3 = False
+                        log.append(f"Error: Intersection between First sets of productions and Follow set is not empty")
+                    else:
+                        log.append(f"Intersection between First sets of productions and Follow set is empty")
+        
+    if not cond3:
+        log.append("Condition 3: No intersection between First sets of productions and Follow set check failed")
+        is_ll1 = False
+    else:
+        log.append("Condition 3: PASSED!")
+
+    log.append("#"*40)
+        
+    # Check if grammar is LL(1)
+    if is_ll1:
+        log.append("Grammar is LL(1)")
+    else:
+        log.append("Grammar is not LL(1)")
+
+    # Write log to file
+    now = datetime.datetime.now()
+    log_file_name = f".\\playground\\ll1_check_{now.day}_{now.month}_{now.hour}_{now.minute}.log"
+    with open(log_file_name, "w+", encoding='utf-8') as f:
+        for line in log:
+            f.write(line + "\n")
+
+def write_log_file(file_path, log_data):
+    with open(file_path, 'w') as file:
+        file.write(log_data)
+
 def main():
-    with open(r"C:\Users\brenn\IdeaProjects\infoh403_project\playground\first.txt", "w+") as f:
-        f.write("First sets:\n")
-        first_sets = {key.name: first(key) for key in grammar}
-        for key, value in first_sets.items():
-            f.write(f"<{key}> : {[item.value[0] if isinstance(item, Enum) else item for item in value]}\n")
-
-    with open(r"C:\Users\brenn\IdeaProjects\infoh403_project\playground\follow.txt", "w+") as f:
-        f.write("\nFollow sets:\n")
-        follow_sets = {key.name: follow(key) for key in grammar}
-        for key, value in follow_sets.items():
-            f.write(f"<{key}> : {[item.value[0] if isinstance(item, Enum) else item for item in value]}\n")
-
+    
+    first_follow_csv()
+    write_log_file(".\\playground\\first_computation_log.txt", "\n".join(logging_first))
+    write_log_file(".\\playground\\follow_computation_log.txt", "\n".join(logging_follow))
     action_table_to_csv()
-    generate_csv()
+    check_ll1()
 
         
 
